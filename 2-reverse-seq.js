f42eb114f8f65eaac6acfc221339551f2ff0797a
Luthfi Zufar <69962163+luthfizufar@users.noreply.github.com>
2024-03-07 17:59:01 +0700

    Update 2-reverse-seq.js

--- 2-reverse-seq.js
+++ 2-reverse-seq.js
@@ -1,9 +1,12 @@
-const reverseSeq = n => {
-    const result=[];
-    for(let i = n; i >= 1; i--){
-        result.push(i);
-    }
-    return result;
+// const reverseSeq = n => {
+    // const result=[];
+    // for(let i = n; i >= 1; i--){
+    //     result.push(i);
+    // }
+    // return result;
+
+reverseSeq =(n) => {
+    return [...Array(n)].map((_el, i) => i + 1).reverse();
 };
 
 console.log(reverseSeq(5))
