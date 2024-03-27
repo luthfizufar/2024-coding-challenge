2ae41c5d80e1c9607807bdeead250699ea7bdf7c
Luthfi Zufar <69962163+luthfizufar@users.noreply.github.com>
2024-03-27 15:45:46 +0700

    Day 16

---
+++ 16-convert-string-to-anArray.js
@@ -0,0 +1,10 @@
+// Day 16
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+function stringToArray(string){
+    return string.split(" ");
+
+}
+
+console.log(stringToArray("Robin Singh"));
+console.log(stringToArray("I love arrays they are my favorite"));
