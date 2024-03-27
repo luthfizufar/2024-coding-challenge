4e3fa8fee7aa29c2cbb1495598115729498b173e
Luthfi Zufar <69962163+luthfizufar@users.noreply.github.com>
2024-03-27 14:09:30 +0700

    Day 14

---
+++ 14-opposite-number.js
@@ -0,0 +1,11 @@
+//Day 14
+//https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+function opposite(number) {
+    return number * -1;
+}
+
+console.log(opposite(0));
+console.log(opposite(4.25));
+console.log(opposite(3.3333333));
+console.log(opposite(-12525220.3325));
